90716d92cd5044ef67883bc51486d94bd49bb55a
Anointing Maxwell <anointingmax@gmail.com>
2024-10-20 09:44:39 +0100

    Basic navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./components";
 
 function App() {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Layout />,
+			children: [
+				{
+					index: true,
+					element: <div>Home</div>,
+				},
+				{
+					path: ":id",
+					element: <div>Details</div>,
+				},
+			],
+		},
+	]);
 
-  return (
-    <>
-
-    </>
-  )
+	return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
