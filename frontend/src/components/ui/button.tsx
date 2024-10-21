187595656e917aa00d460c177cbb6af55cdc12f4
Anointing Maxwell <anointingmax@gmail.com>
2024-10-21 07:50:03 +0100

    More shadcn components

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -12,10 +12,11 @@ const buttonVariants = cva(
 			variant: {
 				default: "bg-default text-black hover:bg-default/90",
 				destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-				outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+				outline: "border border-black/35 bg-background hover:bg-accent hover:text-accent-foreground",
 				secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
+				custom: "",
 			},
 			size: {
 				default: "px-5 py-3",
