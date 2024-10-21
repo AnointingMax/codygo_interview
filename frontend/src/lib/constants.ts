b184f83ca966abfdd17730e3c604573038e2bce5
Anointing Maxwell <anointingmax@gmail.com>
2024-10-21 17:42:56 +0100

    Hotel details page

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,5 @@
 import { AirVent, Cable, Cigarette, Clock1, CloudCog, Dumbbell, EggFried, Martini, PawPrint, Plane, Projector, Sparkles, SquareParking, Telescope, UserPlus, Users, UtensilsCrossed, WashingMachine, Waves, Wifi } from "lucide-react";
+import { Image } from "react-grid-gallery";
 
 export const COUNTRIES = [
    {
@@ -1800,3 +1801,63 @@ export const FEATURE_ICON = {
    "breakfast_included": EggFried,
    "concierge_service": UserPlus
 };
+
+export const images: Image[] = [
+   {
+      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+      width: 320,
+      height: 174,
+   },
+   {
+      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+      width: 320,
+      height: 212,
+   },
+   {
+      src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+      width: 320,
+      height: 212,
+   },
+   {
+      src: "https://c7.staticflickr.com/9/8546/28354329294_bb45ba31fa_b.jpg",
+      width: 320,
+      height: 213,
+   },
+   {
+      src: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg",
+      width: 320,
+      height: 183,
+   },
+   {
+      src: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg",
+      width: 240,
+      height: 320,
+   },
+   {
+      src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
+      width: 320,
+      height: 190,
+   },
+   {
+      src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
+      width: 320,
+      height: 148,
+   },
+   {
+      src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
+      width: 320,
+      height: 213,
+   },
+   {
+      src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
+      alt: "Big Ben - London",
+      width: 248,
+      height: 320,
+   },
+   {
+      src: "https://c7.staticflickr.com/9/8785/28687743710_3580fcb5f0_b.jpg",
+      alt: "Red Zone - Paris",
+      width: 320,
+      height: 113,
+   },
+];
--- frontend/src/pages/Details.tsx
+++ frontend/src/pages/Details.tsx
@@ -1,5 +1,125 @@
+import { FEATURE_ICON, FEATURES, images } from "@/lib/constants";
+import { useState } from "react";
+import { Gallery as GridGallery } from "react-grid-gallery";
+import Lightbox from "yet-another-react-lightbox";
+import StarRatings from "react-star-ratings";
+import "yet-another-react-lightbox/styles.css";
+import { Button } from "@/components/ui/button";
+import { FilePenLine } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { HotelForm } from "@/panels";
+import { HotelType } from "@/types";
+
 const Details = () => {
-	return <div>Details</div>;
+	const hotel: HotelType = {
+		id: 1,
+		name: "Le Meridien",
+		address: "24 Ocean Avenue",
+		city: "Miami",
+		country: "US",
+		rating: 4.2,
+		features: [
+			"free_wifi",
+			"parking",
+			"air_conditioning",
+			"swimming_pool",
+			"fitness_center",
+			"restaurant",
+			"bar",
+			"room_service",
+			"airport_shuttle",
+		],
+		images: [],
+		brands: [],
+		createdAt: "2024-10-20T03:27:45.840Z",
+		updatedAt: "2024-10-20T03:27:45.840Z",
+	};
+
+	return (
+		<div className="grid max-w-6xl gap-6 mx-auto pb-28">
+			<div>
+				<div className="flex items-center gap-2">
+					<h1 className="text-lg font-bold">{hotel.name}</h1>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button variant="custom" className="gap-2 px-0 text-default">
+								<FilePenLine />
+								Edit
+							</Button>
+						</SheetTrigger>
+						<SheetContent className="">
+							<SheetHeader>
+								<SheetTitle>Edit Hotel</SheetTitle>
+							</SheetHeader>
+							<HotelForm />
+						</SheetContent>
+					</Sheet>
+				</div>
+				<div className="flex items-center gap-x-1 gap-y-1 max-w-[650px] flex-wrap">
+					{hotel.brands?.map((brand, index) => (
+						<span key={index} className="flex items-center gap-1 px-2 py-1 bg-black rounded-full">
+							<p className="text-xs font-medium text-white">{brand?.name}</p>
+						</span>
+					))}
+				</div>
+			</div>
+			<Gallery />
+			<div>
+				<h2 className="detail-section">Address</h2>
+				<div className="grid gap-x-4 gap-y-1 grid-cols-[100px,1fr]">
+					<div className="font-medium text-black/90">Street:</div>
+					<div>{hotel?.address}</div>
+					<div className="font-medium text-black/90">City:</div>
+					<div>{hotel?.city}</div>
+					<div className="font-medium text-black/90">Country:</div>
+					<div>{hotel?.country}</div>
+				</div>
+			</div>
+			<div>
+				<h2 className="detail-section">Rating</h2>
+				<StarRatings rating={hotel.rating} starDimension="30px" starSpacing="7px" starRatedColor="var(--default-color)" />
+			</div>
+			<div>
+				<h2 className="detail-section">Hotel Features</h2>
+				<div className="flex items-center mt-1 gap-x-4 gap-y-4 max-w-[650px] flex-wrap">
+					{hotel.features?.map((featureKey, index) => {
+						const Icon = FEATURE_ICON[featureKey];
+						const feature = FEATURES.find((feature) => feature.value === featureKey);
+
+						return (
+							<span key={index} className="flex items-center gap-1 px-4 py-1.5 rounded-full bg-default/20">
+								<Icon className="w-[16px] h-[16px]" />
+								<p className="text-sm font-medium">{feature?.label}</p>
+							</span>
+						);
+					})}
+				</div>
+			</div>
+			<div className="grid gap-1">
+				<span className="text-sm font-medium">Date created: {new Date(hotel.createdAt).toLocaleString()}</span>
+				<span className="text-sm font-medium">Date updated: {new Date(hotel.updatedAt).toLocaleString()}</span>
+			</div>
+		</div>
+	);
+};
+
+const slides = images.map(({ src, width, height }) => ({
+	src,
+	width,
+	height,
+}));
+
+const Gallery = () => {
+	const [index, setIndex] = useState(-1);
+
+	const handleClick = (index: number) => setIndex(index);
+
+	return (
+		<>
+			<GridGallery images={images} onClick={handleClick} enableImageSelection={false} />
+			<Lightbox slides={slides} open={index >= 0} index={index} close={() => setIndex(-1)} />
+		</>
+	);
 };
 
 export default Details;
