55dc22f967496c3c29dc68b1bff638204aaed6d6
Anointing Maxwell <anointingmax@gmail.com>
2024-10-22 11:07:50 +0100

    Deploy to gh pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  base: "codygo-interview",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
