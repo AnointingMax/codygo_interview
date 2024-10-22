5c0c087c6e38d59fa2768cf6a2647752b47c70a4
Anointing Maxwell <anointingmax@gmail.com>
2024-10-22 17:49:38 +0100

    Update cors

--- backend/index.ts
+++ backend/index.ts
@@ -15,6 +15,7 @@ app.use(cors({
 		"http://localhost:3000",
 		"http://localhost:3001",
 		"http://localhost:3002",
+		"https://codygo-interview-frontend.onrender.com"
 	],
 }));
 app.use(express.json())
