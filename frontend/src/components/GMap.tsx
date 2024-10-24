b4feabcb01c9b0b657cf41eac4e69ec909d190ac
Anointing Maxwell <anointingmax@gmail.com>
2024-10-24 13:20:51 +0100

    Revert "Added Map Controls"
    This reverts commit 2d86e8e263788d0a305bc9738ba8cac373ec8e9b.

--- frontend/src/components/GMap.tsx
+++ frontend/src/components/GMap.tsx
@@ -3,9 +3,7 @@ import {
 	ControlPosition,
 	MapControl,
 	AdvancedMarker,
-	ControlPosition,
 	Map,
-	MapControl,
 	useMap,
 	useMapsLibrary,
 	useAdvancedMarkerRef,
@@ -33,8 +31,6 @@ const GMap = ({ coordinates = null, setValues }: Props) => {
 				disableDefaultUI={true}
 			>
 				<AdvancedMarker ref={markerRef} position={coordinates} />
-				<MapControl position={ControlPosition.RIGHT_BOTTOM}>
-			        </MapControl>
 			</Map>
 			{!coordinates && (
 				<MapControl position={ControlPosition.LEFT_CENTER}>
